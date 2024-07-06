99af220df00864f02c0ebd3d341cb653b8c896dc
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2024-07-06 17:49:46 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+
+use std::{
+    collections::VecDeque,
+    fs::File,
+    io::{stdin, stdout, Read, Write},
+};
+
+use um::{IOInterface, UniversalMachine};
+
+mod um;
+
+fn codex() -> Vec<u8> {
+    let mut f = File::open("data/codex.umz").unwrap();
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    buf
+}
+
+fn sandmark() -> Vec<u8> {
+    let mut f = File::open("data/sandmark.umz").unwrap();
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    buf
+}
+
+struct UMIO {
+    input_buffer: VecDeque<u8>,
+}
+
+impl UMIO {
+    fn new(s: &str) -> Self {
+        return UMIO {
+            input_buffer: s.chars().map(|c| c as u8).collect(),
+        };
+    }
+}
+
+impl IOInterface for UMIO {
+    fn request_input(&mut self) -> u8 {
+        eprintln!("Request input!");
+        match self.input_buffer.pop_front() {
+            Some(c) => c,
+            None => {
+                let mut stdin_handle = stdin().lock();
+                let mut byte = [0_u8];
+                stdin_handle.read_exact(&mut byte).unwrap();
+                byte[0]
+            }
+        }
+    }
+
+    fn request_output(&mut self, ch: u8) {
+        stdout().lock().write(&[ch]).unwrap();
+    }
+}
+
+fn main() {
+    let mut io = UMIO::new(r"(\b.bb)(\v.vv)06FHPVboundvarHRAk");
+    let mut um = UniversalMachine::new(&codex(), &mut io).unwrap();
+    um.run();
+}
---
+++ src/um.rs
@@ -0,0 +1,293 @@
+#![allow(dead_code)]
+
+use std::{collections::HashMap, ops::{Index, IndexMut, Div, BitAnd, Not}, usize};
+
+
+pub type Plate = u32;
+
+pub trait IOInterface {
+    fn request_input(&mut self) -> u8;
+    fn request_output(&mut self, ch: u8);
+}
+
+pub struct UniversalMachine<'a> {
+    pub registers: Registers,
+    pub ip: usize,
+    pub arrays: HashMap<Plate, Vec<Plate>>,
+    pub is_halted: bool,
+    pub io: &'a mut dyn IOInterface,
+}
+
+#[derive(Default)]
+pub struct Registers {
+    pub regs: [Plate; 8],
+}
+
+impl Index<RegId> for Registers {
+    type Output = Plate;
+
+    fn index(&self, index: RegId) -> &Self::Output {
+        &self.regs[index as usize]
+    }
+}
+
+impl IndexMut<RegId> for Registers {
+    fn index_mut(&mut self, index: RegId) -> &mut Self::Output {
+        &mut self.regs[index as usize]
+    }
+}
+
+impl <'a> UniversalMachine<'a> {
+    fn plate_from_bytes(bytes: &[u8]) -> Option<Plate> {
+        let bytes = bytes.try_into().ok()?;
+        return Some(Plate::from_be_bytes(bytes))
+    }  
+    
+    pub fn new(program: &[u8],
+               io: &'a mut dyn IOInterface) -> Option<Self> {
+        let program_array: Vec<Plate> = program.chunks(4)
+            .map(UniversalMachine::plate_from_bytes)
+            .collect::<Option<Vec<Plate>>>()?;
+        let registers = Registers::default();
+        let arrays = HashMap::from([(0, program_array)]);
+        Some(UniversalMachine {
+            registers,
+            ip: 0,
+            arrays,
+            io,
+            is_halted: false,
+        })
+    }
+    
+    pub fn run(&mut self) {
+        let mut step = 0;
+        let max_steps = u32::max_value(); 
+        while !self.is_halted && step < max_steps {
+            let command = Command::decode(self.arrays[&0][self.ip]);
+//            eprintln!("{:?}", command);
+            step += 1;
+            self.perform_command(&command);
+            match &command {
+                Command::LoadProg { .. } => {},
+                _ => {
+                    self.ip += 1;
+                }
+            }
+        }
+    } 
+    
+    fn perform_command(&mut self, command: &Command) {
+        match *command {
+            Command::CondMove { dst, src, cnd } => {
+                if self.registers[cnd] != 0  {
+                    self.registers[dst] = self.registers[src]
+                }
+            },
+            Command::ArrLoad { dst, arr, offset } => {
+                let arr = self.registers[arr];
+                let offset = self.registers[offset] as usize;
+                self.registers[dst] = self.arrays[&arr][offset];
+            },
+            Command::ArrStore { src, arr, offset } => {
+                let arr = self.registers[arr];
+                let offset = self.registers[offset] as usize;
+                self.arrays.get_mut(&arr).unwrap()[offset] = self.registers[src];
+            },
+            Command::Add { dst, op1, op2 } => {
+                let op1 = self.registers[op1];
+                let op2 = self.registers[op2];
+                self.registers[dst] = op1.wrapping_add(op2);
+            },
+            Command::Mul { dst, op1, op2 } => {
+                let op1 = self.registers[op1];
+                let op2 = self.registers[op2];
+                self.registers[dst] = op1.wrapping_mul(op2);
+            },
+            Command::Div { dst, op1, op2 } => {
+                let op1 = self.registers[op1];
+                let op2 = self.registers[op2];
+                self.registers[dst] = op1.div(op2);
+            },
+            Command::NotAnd { dst, op1, op2 } => {
+                let op1 = self.registers[op1];
+                let op2 = self.registers[op2];
+                self.registers[dst] = op1.bitand(op2).not();
+            },
+            Command::Halt => {
+                self.is_halted = true;
+            },
+            Command::Alloc { dst, size } => {
+                let next_id = self.arrays.keys().max().unwrap() + 1;
+                let size = self.registers[size] as usize;
+                self.arrays.insert(next_id, vec![0; size]);
+                self.registers[dst] = next_id;
+            },
+            Command::Free { arr } => {
+                let arr = self.registers[arr];
+                self.arrays.remove(&arr);
+            },
+            Command::Output { src } => {
+                let src = self.registers[src];
+                self.io.request_output(src as u8);
+            },
+            Command::Input { dst } => {
+                self.registers[dst] = self.io.request_input() as Plate;
+            },
+            Command::LoadProg { arr, offset } => {
+                let arr = self.registers[arr];
+                let offset = self.registers[offset] as usize;
+                self.arrays.insert(0, self.arrays[&arr].clone());
+                self.ip = offset;
+            },
+            Command::StoreConst { dst, val } => {
+                self.registers[dst] = val;
+            },
+        }
+    }
+}
+
+type RegId = u8;
+
+#[derive(Debug)]
+enum Command {
+    CondMove {
+        dst: RegId,
+        src: RegId,
+        cnd: RegId,
+    },
+    ArrLoad {
+        dst: RegId,
+        arr: RegId,
+        offset: RegId,
+    },
+    ArrStore {
+        src: RegId,
+        arr: RegId,
+        offset: RegId,
+    },
+    Add {
+        dst: RegId,
+        op1: RegId,
+        op2: RegId,
+    },
+    Mul {
+        dst: RegId,
+        op1: RegId,
+        op2: RegId
+    },
+    Div {
+        dst: RegId,
+        op1: RegId,
+        op2: RegId,
+    },
+    NotAnd {
+        dst: RegId,
+        op1: RegId,
+        op2: RegId,
+    },
+    Halt,
+    Alloc {
+        dst: RegId,
+        size: RegId,
+    },
+    Free {
+        arr: RegId,
+    },
+    Output {
+        src: RegId,
+    },
+    Input {
+        dst: RegId,
+    },
+    LoadProg {
+        arr: RegId,
+        offset: RegId
+    },
+    StoreConst {
+        dst: RegId,
+        val: Plate,
+    }
+}
+
+impl Command {
+    fn decode_registers_standard(p: Plate) -> (RegId, RegId, RegId) {
+        let c = (p & 0b111) as RegId;
+        let b = ((p >> 3) & 0b111) as RegId;
+        let a = ((p >> 6) & 0b111) as RegId;
+        (a, b, c)
+    }
+    
+    fn decode_special(p: Plate) -> (RegId, Plate) {
+        let a = ((p >> 25) & 0b111) as RegId;
+        let v = p & 0b1111111111111111111111111;
+        (a, v)
+    }
+    
+    fn decode_command_id(p: Plate) -> u8 {
+        ((p >> 28) & 0b1111) as u8
+    }
+    
+    pub fn decode(p: Plate) -> Command {
+        match Command::decode_command_id(p) {
+            0  => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::CondMove { src: b, dst: a, cnd: c }
+            },
+            1 => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::ArrLoad { dst: a, arr: b, offset: c }
+            }
+            2 => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::ArrStore { src: c, arr: a, offset: b }
+            }
+            3 => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::Add { dst: a, op1: b, op2: c }
+            }
+            4 => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::Mul { dst: a, op1: b, op2: c }
+            }
+            5 => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::Div { dst: a, op1: b, op2: c }
+            }
+            6 => {
+                let (a, b, c) = Command::decode_registers_standard(p);
+                Command::NotAnd { dst: a, op1: b, op2: c }
+            }
+            7 => {
+                Command::Halt
+            }
+            8 => {
+                let (_a, b, c) = Command::decode_registers_standard(p);
+                Command::Alloc { dst: b, size: c }
+            }
+            9 => {
+                let (_a, _b, c) = Command::decode_registers_standard(p);
+                Command::Free { arr: c }
+            }
+            10 => {
+                let (_a, _b, c) = Command::decode_registers_standard(p);
+                Command::Output { src: c }
+            }
+            11 => {
+                let (_a, _b, c) = Command::decode_registers_standard(p);
+                Command::Input { dst: c }
+            }
+            12 => {
+                let (_a, b, c) = Command::decode_registers_standard(p);
+                Command::LoadProg { arr: b, offset: c }
+            }
+            13 => {
+                let (a, v) = Command::decode_special(p);
+                Command::StoreConst { dst: a, val: v }
+            }
+
+            _ => {
+                unreachable!()
+            }
+        }
+    }
+}
